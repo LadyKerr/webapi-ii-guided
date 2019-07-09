8041c099ee3b66a3d10c818f0d8042aacf472db6
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-09 05:00:32 -0400

    wip

--- index.js
+++ index.js
@@ -122,6 +122,8 @@ server.put('/api/hubs/:id', async (req, res) => {
 });
 
 // add an endpoint that returns all the messages for a hub
+// we should use findHubMessages inside hubs-model
+
 // add an endpoint for adding new message to a hub
 
 server.listen(4000, () => {
